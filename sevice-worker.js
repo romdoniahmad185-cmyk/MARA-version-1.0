30c77c1ac3eefd57b5b0109d973d17d06e692025
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-11 01:42:22 +0700

    Add files via upload

---
+++ sevice-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "mara-os-v0.1";
+
+const FILES_TO_CACHE = [
+  "./lock-screen.html",
+  "./home.html",
+  "./rama.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
