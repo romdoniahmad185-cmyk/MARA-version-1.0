96b448262e74d5bd272414e0c54d7ef6541ed893
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-11 17:04:22 +0700

    Update sevice-worker.js

--- sevice-worker.js
+++ sevice-worker.js
@@ -1,40 +1,30 @@
-const CACHE_NAME = "mara-os-v0.1";
+const CACHE_NAME = "mara-os-v1";
 
 const FILES_TO_CACHE = [
-  "./looc-screen.html",
-  "./home.html",
-  "./app-drawer.html",
-  "./manifest.json"
+    "./",
+    "./index.html",
+    "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
 
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-
-  self.clients.claim();
+    event.waitUntil(
+        self.clients.claim()
+    );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
